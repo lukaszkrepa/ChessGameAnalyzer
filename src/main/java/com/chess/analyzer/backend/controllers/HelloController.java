0269f9af96c6d609282ae25c831d41ab7aa2ee5b
LukaszKrepa <lukaszkrepa@yahoo.com>
2025-05-29 00:50:40 +0200

    Added lambda handler test controller and sam configuration

---
+++ src/main/java/com/chess/analyzer/backend/controllers/HelloController.java
@@ -0,0 +1,16 @@
+package com.chess.analyzer.backend.controllers;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/pingpong")
+    public ResponseEntity<String> ping() {
+        return ResponseEntity.ok("pong");
+    }
+}
+
