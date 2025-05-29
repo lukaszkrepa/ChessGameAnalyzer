879c9cc0312fad9e4363fd07e1dfa38f75693f6d
LukaszKrepa <lukaszkrepa@yahoo.com>
2025-05-29 11:15:12 +0200

    Fixed requested changes

--- src/main/java/com/chess/analyzer/backend/controllers/HelloController.java
+++ src/main/java/com/chess/analyzer/backend/controllers/HelloController.java
@@ -1,6 +1,4 @@
 package com.chess.analyzer.backend.controllers;
-
-import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
